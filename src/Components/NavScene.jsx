97c3ff7905a207c6b8e1a32a2d48ae70bf9dc31a
Tristan <tristan@bunyan.com>
2018-05-28 20:20:04 +0100

    scenes appear in nice places now

--- src/Components/NavScene.jsx
+++ src/Components/NavScene.jsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { Scene } from "./Scene";
 // import { Inventory } from "../Classes/Inventory";
 import { Game } from "./Game";
+import _ from "underscore";
 
 export class NavSceneProps {
     children: Scene[]
@@ -36,14 +37,42 @@ export class NavScene extends Scene {
         return this.state.activeScene === this.getSceneID();
     }
 
+    getClassNameAppendage(): string{
+        switch(this.getNumActiveChildren()){
+            case 1:
+            return "one";
+            case 2:
+            return "two";
+            case 3:
+            return "three";
+            case 4:
+            case 5:
+            return "four-to-five";
+            default: 
+            return "six-to-nine"
+        }
+    }
+
     setActiveScene(scene: any) {
         this.setState({activeScene: scene.getSceneID()})
     }
+
+    getNumActiveChildren(){
+
+        let count = 0;
+		this.props.children.forEach((child, index) => {
+            if (this.props.game.state.gameState[index]){
+                count ++;
+            }            
+        })
+        return count;
+	}
     
     render() {
         // const class = this.props.active
-        let className: string = "scene one-of-four ";
+        let className: string = "scene nav-scene ";
         className += this.isActive() ? "active " : "transient ";
+        className += this.getClassNameAppendage();
 
  
         const { children} = this.props; 
